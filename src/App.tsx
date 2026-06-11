88ccc69db3037919cfc796d8044da40682edf60f
Ujahcollins <ujahcollins@gmail.com>
2026-06-11 22:26:14 +0100

    update on app

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import Navbar from "./component/Navbar";
 import Home from "./pages/Home";
 import Invite from "./pages/Invite";
 import Registry from "./pages/Registry";
-import Vendors from "./Pages/Vendors";
-import Locationz from "./Pages/Locationz";
-import Galleries from "./Pages/Galleries";
+import Vendors from "./pages/Vendors";
+import Locationz from "./pages/Locationz";
+import Galleries from "./pages/Galleries";
 import Layout from "./component/Layout";
 
 function App() {
